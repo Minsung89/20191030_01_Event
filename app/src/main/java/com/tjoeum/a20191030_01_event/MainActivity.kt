719e8a822c5e65993f3ff33bacf46698ba2853e1
Minsung89 <minsung0217@gmail.com>
2019-10-30 20:53:22 +0900

    이벤트 처리 연습 ArrayList 연습 조건문 연습

--- app/src/main/java/com/tjoeum/a20191030_01_event/MainActivity.kt
+++ app/src/main/java/com/tjoeum/a20191030_01_event/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeum.a20191030_01_event
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -19,5 +20,59 @@ class MainActivity : AppCompatActivity() {
 
         firstTextView.text = "실행 후"
 
+        okBtn.setOnClickListener {
+            firstTextView.text = "확인버튼 클릭됨"
+        }
+
+        okBtn.setOnLongClickListener {
+            firstTextView.text = "확인버튼 롱클릭"
+            return@setOnLongClickListener true
+        }
+
+//        @라벨에 대한 예시. 책 49p
+
+        firstFor@ for (i in 0..10){
+            secondFor@ for (j in 0..10){
+                if(j==2){
+                    break@firstFor
+                }
+            }
+        }
+
+        var stringList:ArrayList<String> = ArrayList()
+
+//        "또니" 글자를 추가, "황진호", "구민성" 추가
+
+        stringList.add("또니")
+        stringList.add("황진호")
+        stringList.add("구민성")
+
+        var intList:ArrayList<Int> = ArrayList()
+        intList.add(10)
+        intList.add(30)
+        intList.add(50)
+
+//        구민성은 몇번째로 넣었을까? 어느 위치에?
+        var indexOfKoo = stringList.indexOf("구민성")
+        Log.d("구민성님의 위치", String.format("%d", indexOfKoo))
+
+//        조경진을 제거
+        stringList.remove("조경진")
+
+        var a =10
+        var b =20
+
+        var c = if (a==b){
+            Log.d("로그", "둘이 같다")
+        } else {
+            Log.d("로그", "둘이 다르다")
+        }   // 이런 문법도 가능함. 책 39p
+
+        when(a) {
+            0, 1 -> Log.d("when연습", "a가 0이나 1이다")
+            in 2..5 -> Log.d("when연습", "a가 2~5 사이다")
+            in intList -> Log.d("when연습", "a가 intList에 포함")
+            !in intList -> Log.d("when연습", "a가 intList에 불포함")
+        }
     }
 }
